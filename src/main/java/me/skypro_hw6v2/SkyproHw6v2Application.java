b4d76e08133a5f7ce30732d8d245401bfb3cd1cd
dasha <dasha.motry2002@gmail.ru>
2024-08-21 18:10:04 +0300

    Initial commit

---
+++ src/main/java/me/skypro_hw6v2/SkyproHw6v2Application.java
@@ -0,0 +1,12 @@
+package me.skypro_hw6v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproHw6v2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproHw6v2Application.class, args);
+	}
+}
---
+++ src/main/java/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package service;
+import java.util.List;
+
+public class EmployeeService {
+
+//public class PersonServiceImpl implements PersonService {
+//    List<Person> persons = List.of(
+//    )
+//}
+
+public String addEmployee() {
+return "дописать код добавления персонала";
+}
+
+public String removeEmployee() {
+    return "дописать код удаления человека";
+}
+
+public String findEmployee() {
+    return "дописать код поиска человека";
+}
+
+}
